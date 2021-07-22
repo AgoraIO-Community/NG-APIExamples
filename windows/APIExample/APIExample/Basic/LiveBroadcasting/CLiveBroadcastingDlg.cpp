9267ced052b88a8521a3e3f3ff2b638d84f96066
sbd021 <sbd021@126.com>
2021-07-22 10:14:10 +0800

    windows set report in call before join

--- windows/APIExample/APIExample/Basic/LiveBroadcasting/CLiveBroadcastingDlg.cpp
+++ windows/APIExample/APIExample/Basic/LiveBroadcasting/CLiveBroadcastingDlg.cpp
@@ -6,83 +6,6 @@
 #include "CLiveBroadcastingDlg.h"
 //#include "CAgoraReportInCallDlg.h"
 #include <iostream>
-/*
-note:
-    Join the channel callback.This callback method indicates that the client
-    successfully joined the specified channel.Channel ids are assigned based
-    on the channel name specified in the joinChannel. If IRtcEngine::joinChannel
-    is called without a user ID specified. The server will automatically assign one
-parameters:
-    channel:channel name.
-    uid: user ID¡£If the UID is specified in the joinChannel, that ID is returned here;
-    Otherwise, use the ID automatically assigned by the Agora server.
-    elapsed: The Time from the joinChannel until this event occurred (ms).
-*/
-/*void CLiveBroadcastingRtcEngineEventHandler::onJoinChannelSuccess(const char* channel, uid_t uid, int elapsed)
-{
-    if (m_hMsgHanlder) {
-        ::PostMessage(m_hMsgHanlder, WM_MSGID(EID_JOINCHANNEL_SUCCESS), (WPARAM)uid, (LPARAM)elapsed);
-    }
-}*/
-
-/*
-note:
-    In the live broadcast scene, each anchor can receive the callback
-    of the new anchor joining the channel, and can obtain the uID of the anchor.
-    Viewers also receive a callback when a new anchor joins the channel and
-    get the anchor's UID.When the Web side joins the live channel, the SDK will
-    default to the Web side as long as there is a push stream on the
-    Web side and trigger the callback.
-parameters:
-    uid: remote user/anchor ID for newly added channel.
-    elapsed: The joinChannel is called from the local user to the delay triggered
-    by the callback£¨ms).
-*/
-/*void CLiveBroadcastingRtcEngineEventHandler::onUserJoined(uid_t uid, int elapsed) {
-    if (m_hMsgHanlder) {
-        ::PostMessage(m_hMsgHanlder, WM_MSGID(EID_USER_JOINED), (WPARAM)uid, (LPARAM)elapsed);
-    }
-}*/
-
-/*
-note:
-    Remote user (communication scenario)/anchor (live scenario) is called back from
-    the current channel.A remote user/anchor has left the channel (or dropped the line).
-    There are two reasons for users to leave the channel, namely normal departure and
-    time-out:When leaving normally, the remote user/anchor will send a message like
-    "goodbye". After receiving this message, determine if the user left the channel.
-    The basis of timeout dropout is that within a certain period of time
-    (live broadcast scene has a slight delay), if the user does not receive any
-    packet from the other side, it will be judged as the other side dropout.
-    False positives are possible when the network is poor. We recommend using the
-    Agora Real-time messaging SDK for reliable drop detection.
-parameters:
-    uid: The user ID of an offline user or anchor.
-    reason:Offline reason: USER_OFFLINE_REASON_TYPE.
-*/
-/*void CLiveBroadcastingRtcEngineEventHandler::onUserOffline(uid_t uid, USER_OFFLINE_REASON_TYPE reason)
-{
-    if (m_hMsgHanlder) {
-        ::PostMessage(m_hMsgHanlder, WM_MSGID(EID_USER_OFFLINE), (WPARAM)uid, (LPARAM)reason);
-    }
-}*/
-
-/*
-note:
-    When the App calls the leaveChannel method, the SDK indicates that the App
-    has successfully left the channel. In this callback method, the App can get
-    the total call time, the data traffic sent and received by THE SDK and other
-    information. The App obtains the call duration and data statistics received
-    or sent by the SDK through this callback.
-parametes:
-    stats: Call statistics.
-*/
-/*void CLiveBroadcastingRtcEngineEventHandler::onLeaveChannel(const RtcStats& stats)
-{
-    if (m_hMsgHanlder) {
-        ::PostMessage(m_hMsgHanlder, WM_MSGID(EID_LEAVE_CHANNEL), 0, 0);
-    }
-}*/
 
 // CLiveBroadcastingDlg dialog
 IMPLEMENT_DYNAMIC(CLiveBroadcastingDlg, CDialogEx)
@@ -687,6 +610,10 @@ LRESULT CLiveBroadcastingDlg::OnEIDJoinChannelSuccess(WPARAM wParam, LPARAM lPar
 		m_videoWnds[i].SetStrmId(strmInfos[i].stream_id);
 		m_strmIds.insert(strmInfos[i].stream_id);
 	}
+
+	if (m_chkReportInCall.GetCheck()) {
+		OnClickedCheckReportIncall();
+	}
    
     //notify parent window
     ::PostMessage(GetParent()->GetSafeHwnd(), WM_MSGID(EID_JOINCHANNEL_SUCCESS), TRUE, 0);
@@ -986,7 +913,7 @@ void CLiveBroadcastingDlg::OnCustomdrawSliderSpeaker(NMHDR *pNMHDR, LRESULT *pRe
 LRESULT  CLiveBroadcastingDlg::OnEIDLocalStreamStats(WPARAM wParam, LPARAM lParam)
 {
 	PLocalStats stats = (PLocalStats)wParam;
-	if (!m_chkReportInCall.GetCheck()) {
+	if (m_chkReportInCall.GetCheck()) {
 		for (int i = 0; i < m_strmIds.size(); ++i) {
 			if (m_videoWnds[i].GetStrmId().compare(stats->strmid) == 0) {
 				m_videoWnds[i].SetLocalStats(*stats);
@@ -1003,7 +930,7 @@ LRESULT  CLiveBroadcastingDlg::OnEIDLocalStreamStats(WPARAM wParam, LPARAM lPara
 LRESULT  CLiveBroadcastingDlg::OnEIDRemoteStreamStats(WPARAM wParam, LPARAM lParam)
 {
 	PRemoteStats stats = (PRemoteStats)wParam;
-	if (!m_chkReportInCall.GetCheck()) {
+	if (m_chkReportInCall.GetCheck()) {
 		for (int i = 0; i < m_strmIds.size(); ++i) {
 			if (m_videoWnds[i].GetStrmId().compare(stats->strmid) == 0) {
 				m_videoWnds[i].SetRemoteStats(*stats);
